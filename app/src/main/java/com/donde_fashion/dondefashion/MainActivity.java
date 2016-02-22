17d30406eaaced5ecfc037b70eff929b9c572b36
Dekel Naar <dekelna@fb.com>
2016-02-23 00:45:35 +0200

    supporting back button

--- app/src/main/java/com/donde_fashion/dondefashion/MainActivity.java
+++ app/src/main/java/com/donde_fashion/dondefashion/MainActivity.java
@@ -2,6 +2,7 @@ package com.donde_fashion.dondefashion;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -10,15 +11,34 @@ import android.webkit.WebViewClient;
  */
 public class MainActivity extends Activity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView) findViewById(R.id.webView);
+        webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://search.dev.donde-app.com/search");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
